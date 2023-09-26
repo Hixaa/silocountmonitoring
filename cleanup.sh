7bd2f7d682321a5e8dcac916e8430d6098dcb2b3
Siddhesh_Raut <84245560+SiddheshSR7@users.noreply.github.com>
2023-09-26 15:07:26 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Performing Cleanup"
-sudo find /home/pi/scms/code/components -name "*.pyc" -exec rm -f {} \;
+sudo find /home/pi/silocountmonitoring/scms/code/components -name "*.pyc" -exec rm -f {} \;
