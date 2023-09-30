9923fd5bed6d1af2d69e716048d185b54b29b79c
Siddhesh_Raut <84245560+SiddheshSR7@users.noreply.github.com>
2023-09-30 13:16:03 +0530

    Delete replace.sh

--- replace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "[+] Replacing config_scms.ini"
-sudo cp -f https://raw.githubusercontent.com/Hixaa/silocountmonitoring/main/scms/config_scms.ini /
-
-echo "[+] Replacing components folder"
-sudo cp -f  https://raw.githubusercontent.com/Hixaa/silocountmonitoring/main/scms/components/ /
